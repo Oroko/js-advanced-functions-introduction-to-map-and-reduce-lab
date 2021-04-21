8ff0b9ba63d3ca4308a19d881d54c836b8058637
Oroko <aoroko09@gmail.com>
2021-04-21 18:29:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+  return sourceArray.map(n => n * -1)
+}
+const mapToNoChange = (arr) => {
+  return arr.map(n => n)
+}
+const mapToDouble = (arr) => {
+  return arr.map(n => n * 2)
+}
+const mapToSquare = (arr) => {
+  return arr.map(n => n ** 2)
+} 
+
+const reduceToTotal = (arr, startingPoint=0) => {
+ return arr.reduce((acc,currentValue) =>{
+   return acc + currentValue
+ },startingPoint)
+}
+
+const reduceToAllTrue = (arr) => {
+  return arr.reduce((acc,currentValue) => {
+    return acc && Boolean(currentValue);
+  }, true)
+}
+const reduceToAnyTrue = (arr) => {
+  return arr.reduce((acc, currentValue) => acc || Boolean(currentValue),false)
+}
