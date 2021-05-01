b9f40e56224ca6c49fae1d531315bd77f25d0431
Oroko <aoroko09@gmail.com>
2021-05-01 14:33:18 -0400

    commit tree 9f9840740cc765693828935f9514dd22f16dc8d0

--- index.js
+++ index.js
@@ -1,28 +1,59 @@
 // Your code here
-const mapToNegativize = (sourceArray) => {
-  return sourceArray.map(n => n * -1)
-}
-const mapToNoChange = (arr) => {
-  return arr.map(n => n)
-}
-const mapToDouble = (arr) => {
-  return arr.map(n => n * 2)
-}
-const mapToSquare = (arr) => {
-  return arr.map(n => n ** 2)
-} 
+// const mapToNegativize = (sourceArray) => {
+//   return sourceArray.map(n => n * -1)
+// }
 
-const reduceToTotal = (arr, startingPoint=0) => {
- return arr.reduce((acc,currentValue) =>{
-   return acc + currentValue
- },startingPoint)
-}
+const mapToNegativize = (sourceArray) => {
+  let newValues = [];
+  sourceArray.forEach((n) => {
+    newValues.push(n * -1);
+  });
+  return newValues;
+};
+// const mapToNoChange = (arr) => {
+//   return arr.map(n => n)
+// }
+
+const mapToNoChange = (arr) => {
+  let sameValues = [];
+  arr.forEach((n) => {
+    sameValues.push(n);
+  });
+  return sameValues;
+};
+// const mapToDouble = (arr) => {
+//   return arr.map(n => n * 2)
+// }
+const mapToDouble = (arr) => {
+  let doubledValues = [];
+  arr.forEach((n) => {
+    doubledValues.push(n * 2);
+  });
+  return doubledValues;
+};
+// const mapToSquare = (arr) => {
+//   return arr.map(n => n ** 2)
+// }
+
+const mapToSquare = (arr) => {
+  let squaredValues = [];
+  for (const n of arr) {
+    squaredValues.push(n * n);
+  }
+  return squaredValues;
+};
+
+const reduceToTotal = (arr, startingPoint = 0) => {
+  return arr.reduce((acc, currentValue) => {
+    return acc + currentValue;
+  }, startingPoint);
+};
 
 const reduceToAllTrue = (arr) => {
-  return arr.reduce((acc,currentValue) => {
+  return arr.reduce((acc, currentValue) => {
     return acc && Boolean(currentValue);
-  }, true)
-}
+  }, true);
+};
 const reduceToAnyTrue = (arr) => {
-  return arr.reduce((acc, currentValue) => acc || Boolean(currentValue),false)
-}
+  return arr.reduce((acc, currentValue) => acc || Boolean(currentValue), false);
+};
